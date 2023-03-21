d9f2a119e676cac9dd3f705128f174a678a5f5ef
30omdaa <mmm.emad64@gmail.com>
2023-03-21 10:45:11 +0200

    add new type

--- src/components/redux/actions/Types.js
+++ src/components/redux/actions/Types.js
@@ -2,3 +2,4 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const INC_BY_VALUE = 'INC_BY_VALUE';
 export const DEC_BY_VALUE = 'DEC_BY_VALUE';
+export const INCREMENT_1 = 'INCREMENT_1';
